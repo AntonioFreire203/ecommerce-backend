c447e54ad597653bf3a11233adfb9245430828c5
AntonioFreire203 <freireantonio203@gmail.com>
2025-06-05 23:35:48 -0300

    test(categoria): cobre inserção, duplicação, exclusão e atualização
    - Valida inserção com nome inválido e duplicado
    - Realiza busca, exclusão e atualização
    - Garante consistência do banco após as ações

--- src/scripts/testarCategoria.js
+++ src/scripts/testarCategoria.js
@@ -1,40 +1,47 @@
 const Categoria = require("../models/categoria");
 
 async function testarCategoria() {
-    // Teste de inserção de categoria
+  // Teste: Inserção válida
   console.log("Teste: inserção válida");
-  let cat1 = new Categoria("Informática", "Produtos relacionados a computadores e periféricos");
+  const cat1 = new Categoria("Informática", "Produtos relacionados a computadores e periféricos");
   await cat1.inserir();
- // Teste de inserção com nome inválido (vazio)
+
+  // Teste: Nome inválido (vazio)
   console.log("Teste: nome inválido (vazio)");
+  const catInvalida = new Categoria("", "Sem nome válido");
+  await catInvalida.inserir();
+
+  // Teste: Inserção duplicada
   console.log("Teste: inserção duplicada");
-  let catDuplicada = new Categoria("Informática", "Descrição alternativa");
+  const catDuplicada = new Categoria("Informática", "Descrição alternativa");
   await catDuplicada.inserir();
-//
+
+  // Buscar categorias
   console.log("Teste: buscar categorias");
   await Categoria.buscar();
 
+  // Inserção de categoria para exclusão
+  const cat2 = new Categoria("Beleza", "Produtos de cuidados pessoais e cosméticos");
+  await cat2.inserir();
 
- let cat2 = new Categoria("Beleza ", "Produtos de cuidados pessoais e cosméticos");
-  await cat2.inserir(); 
-
-//deletar categoria de teste
+  // Exclusão de categoria
   console.log("Teste: exclusão de categoria");
-  await Categoria.deletar({ nome: "Beleza " });
+  await Categoria.deletar({ nome: "Beleza" });
 
+  // Buscar após exclusão
   console.log("Teste: buscar categorias após exclusão");
   await Categoria.buscar();
 
-  // Atualizar categoria
-  
+  // Atualização
   console.log("Teste: atualizar categoria");
-    await Categoria.atualizar(
+  await Categoria.atualizar(
     { nome: "Informática" },
     { descricao: "Tecnologia, computadores e acessórios" }
-    );
-        console.log("Teste: buscar categorias após atualização");
-        await Categoria.buscar();
+  );
 
+  // Buscar após atualização
+  console.log("Teste: buscar categorias após atualização");
+  await Categoria.buscar();
 }
 
 testarCategoria();
